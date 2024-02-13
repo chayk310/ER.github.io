2f70c5c32b56a36ecfc73b1e9e6f4d7bdc0cb267
chayk310 <46476765+chayk310@users.noreply.github.com>
2024-02-13 09:14:38 +0900

    Update imageSlide.js

--- assets/imageSlide.js
+++ assets/imageSlide.js
@@ -1,41 +1,34 @@
- window.onload = function() {
-    const kindWrap = document.querySelector('.slide');
-    const slider = kindWrap.querySelector('.slider_img');
-    const slideLis = slider.querySelectorAll('li')
-    const moveButton = kindWrap.querySelector('.arrow');
+window.onload = function() {
+  const kindWrap =  document.querySelector('.kind_wrap');
+  const slider = kindWrap.querySelector('.slider');
+  const slideLis = slider.querySelectorAll('li')
+  const moveButton = kindWrap.querySelector('.arrow');
 
-    /* 주요 변수 초기화 */  
-    let currentIdx = 0;
-    let translate = 0;
-    const speedTime = 500;
+  /* ul 넓이 계산해 주기 */
+  const liWidth = slideLis[0].clientWidth;
+  const sliderWidth = liWidth * slideLis.length;
+  slider.style.width = `${sliderWidth}px` ;
 
-    /* CSSOM 셋업 */
-    const liWidth = slideLis[0].clientWidth;
-    const sliderWidth = liWidth * slideLis.length;
-    slider.style.width = `${sliderWidth}px` ;
+  /* 리스너 설치하기 */
+  let currentIdx = 0; // 슬라이드 현재 번호
+  let translate = 0; // 슬라이드 위치 값
+  moveButton.addEventListener('click', moveSlide);
 
-    /* 리스너 설치하기 */
-    moveButton.addEventListener('click', moveSlide);
-
-    /* 슬라이드 실행 */
-    function move(D) {
-      currentIdx += (-1 * D);
-      translate += liWidth * D;
-      slider.style.transform = `translateX(${translate}px)`;
-      slider.style.transition = `all ${speedTime}ms ease`
-  }
-
-
-    /* 버튼 클릭 */
-    function moveSlide(event) {
-      event.preventDefault();
-      if (event.target.className === 'next') {
-        if (currentIdx === slideLis.length -1) return;
-          move(-1);
-      } else {
-          if (currentIdx === 0) return;
-            move(1);
+  function moveSlide(event) {
+    event.preventDefault();
+    if (event.target.className === 'next') {
+      if (currentIdx !== slideLis.length -1) {
+        translate -= liWidth;
+        slider.style.transform = `translateX(${translate}px)`;
+        currentIdx += 1;
+      }
+    } else if (event.target.className === 'prev') {
+        if (currentIdx !== 0) {
+          translate += liWidth;
+          slider.style.transform = `translateX(${translate}px)`;
+          currentIdx -= 1;
         }
-    }
-
+      }
   }
+
+}
